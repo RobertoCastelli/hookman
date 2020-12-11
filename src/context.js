671faaa98fd663f7471c59ac5853ad3d0be7b597
RobertoCastelli <roberto.castelli.teal@gmail.com>
2020-12-11 21:55:35 +0100

    fixed core guess

--- src/context.js
+++ src/context.js
@@ -2,32 +2,32 @@ import React, { useState } from "react"
 export const ContextData = React.createContext()
 
 const ContextProvider = (props) => {
-	const letters = "abcdefghilmnopqrstuvz".split("")
+	const letters = "abcdefghijklmnopqrstuvwxyz".split("")
 	const numbers = "1234567890".split("")
 	const titles = ["javascript", "monkey", "amazing", "pancake"]
 
-	const [title, setTitle] = useState("guess the movie title")
+	const [title, setTitle] = useState("")
 	const [criptedTitle, setCriptedTitle] = useState("")
 
 	// GET the movie TITLE and CRIPT it
 	const criptTitle = () => {
 		const titleTemp = titles[Math.floor(Math.random() * titles.length)]
-		const criptTemp = [...titleTemp].map((c) => (c = "_")).join(" ")
+		const criptTemp = [...titleTemp].map((c) => (c = "_"))
 		setTitle(titleTemp)
 		setCriptedTitle(criptTemp)
 	}
 	console.log(title)
 
 	// COMPARE guessedLetter & UPDATE the movie TITLE
-	const updateTitle = (guessedChar) => {
-		let criptTemp = [...criptedTitle].map((c) => {
-			if (c === guessedChar) {
-				console.log(c)
-			} else {
-				console.log("no")
-			}
+	const updateTitle = (guessedLetter) => {
+		let criptTemp = [...criptedTitle]
+		let titleTemp = [...title]
+		titleTemp.forEach((c, id) => {
+			guessedLetter === c && (criptTemp[id] = c)
+			setCriptedTitle(criptTemp)
 		})
-		setCriptedTitle(criptTemp)
+
+		console.log(criptTemp)
 	}
 
 	return (
